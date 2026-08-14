764bcf72625c7284acc4eac457ae5aa2238a6b8a
ankitagore-45 <ankitagore35@gmail.com>
2026-08-14 10:35:23 +0530

    active

---
+++ src/main/java/com/ankita/HospitalManagementProjApplication.java
@@ -0,0 +1,13 @@
+package com.ankita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementProjApplication.class, args);
+	}
+
+}
